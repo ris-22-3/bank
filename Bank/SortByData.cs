ccd2ff5ab8261858fc4c437177461fa7c8bf7037
madvicspar <vmakkuzina@edu.hse.ru>
2023-06-04 16:10:15 +0500

    Revert "Revert "заполнение datagridview, снятие и внесение наличных""
    This reverts commit d851f4ab567656a7132fc0fb29576e01bf906c19.

---
+++ Bank/SortByData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public class SortByData : IComparer
+    {
+        public int Compare(object? x, object? y)
+        {
+            Operation s1 = x as Operation;
+            Operation s2 = y as Operation;
+            if (s1 == null || s2 == null)
+                return 0;
+            if (s1.date == s2.date)
+                return 0;
+            if (s1.date > s2.date)
+                return 1;
+            return -1;
+        }
+    }
+}
