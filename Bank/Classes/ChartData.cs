4bfa0de30e9318909d912acde1ca476a8aa58a5c
Сергей Уткин <su29638@gmail.com>
2023-06-18 20:03:11 +0500

    Почистил код

--- Bank/Classes/ChartData.cs
+++ Bank/Classes/ChartData.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Unicode;
-using System.Threading.Tasks;
 using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Bank
